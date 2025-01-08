ea57db145cb11bd06b416fa45e625e543bc48e07
Krishna Shah <krishnashah131103@gmail.com>
2025-01-08 16:25:53 +0530

    Create 324.py

---
+++ 324.py
@@ -0,0 +1,21 @@
+#I tried solving it using the swapping method but the downside to it was the repetitive numbers
+#This approach is equally easy to understand
+
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
+
+        n = len(nums)
+        mid = (n + 1) // 2
+
+        left = nums[:mid][::-1]
+        right = nums[mid:][::-1]
+
+        for i in range(n):
+            if i % 2 == 0:
+                nums[i] = left.pop(0)
+            else:
+                nums[i] = right.pop(0)
