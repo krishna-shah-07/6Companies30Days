c77823f6b71eaae0f1e0016d2206e7e17f2def0d
Krishna Shah <krishnashah131103@gmail.com>
2025-01-21 11:48:51 +0530

    Create 845.py

---
+++ 845.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestMountain(self, arr: List[int]) -> int:
+        n = len(arr)
+        if n < 3:
+            return 0
+        
+        longest = 0
+        i = 1  
+        
+        while i < n - 1:
+            if arr[i - 1] < arr[i] > arr[i + 1]:
+                left = i - 1
+                while left > 0 and arr[left - 1] < arr[left]:
+                    left -= 1
+                
+                right = i + 1
+                while right < n - 1 and arr[right] > arr[right + 1]:
+                    right += 1
+                
+                longest = max(longest, right - left + 1)
+                i = right
+            else:
+                i += 1
+        
+        return longest
